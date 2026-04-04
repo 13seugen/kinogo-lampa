b2c0e084ebf411202534a2eeacaaf62c6d9aa084
13seugen <seugen@seugen.com>
2026-04-04 16:03:59 +0200

    Add files via upload

--- kinogo.js
+++ kinogo.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var PLUGIN_VERSION = '20260404-25';
+    var PLUGIN_VERSION = '20260404-26';
     if (window.kinogo_source_plugin_version === PLUGIN_VERSION) return;
     window.kinogo_source_plugin_version = PLUGIN_VERSION;
 
@@ -9,7 +9,7 @@
     var SOURCE_TITLE = 'KinoGO';
     var BASE_URL = 'https://kinogo.ec';
     var MIRROR_BASES = ['https://kinogo.mu', 'https://kinogo.luxury'];
-    var TEST_PROXY_URL = 'https://cors.eu.org/';
+    var TEST_PROXY_URL = '';
     var CACHE_MINUTES = 45;
     var REQUEST_TIMEOUT = 25000;
 
@@ -2375,7 +2375,6 @@
 
     function start() {
         try {
-            if (window.Lampa && Lampa.Storage) Lampa.Storage.set('kinogo_proxy', '');
             ensureMainSourceNotKinogo();
             hideKinogoInSourceSettings();
             register();
